e40109726093e7b02d2e574bc59b9427bccb78d4
VIEGAS Mickael <mviegas@excilys.com>
2016-04-21 18:06:37 +0200

    MultiPart

--- dashboard-server/dashboard-server-webapp/src/main/java/com/excilys/shoofleurs/dashboard/webapp/rest/ContentResource.java
+++ dashboard-server/dashboard-server-webapp/src/main/java/com/excilys/shoofleurs/dashboard/webapp/rest/ContentResource.java
@@ -5,14 +5,17 @@ import com.excilys.shoofleurs.dashboard.webapp.rest.json.Response;
 import com.excilys.shoofleurs.dashboard.webapp.rest.json.mapper.JsonMapper;
 import com.excilys.shoofleurs.dashboard.business.service.DiaporamaService;
 import com.excilys.shoofleurs.dashboard.json.Views;
+import org.glassfish.jersey.media.multipart.ContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataBodyPart;
+import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.io.*;
+import java.net.URI;
+import java.net.URLEncoder;
 
 @Stateless
 @Path("contents")
@@ -54,4 +57,56 @@ public class ContentResource {
 		}
 		return new Response("endNull", 500);
 	}*/
+
+	@GET
+	@Path("test")
+	public javax.ws.rs.core.Response test() {
+		return javax.ws.rs.core.Response.ok("Salut").build();
+	}
+
+	@POST
+	@Path("upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public javax.ws.rs.core.Response setImageProfielPicture(FormDataMultiPart form) {
+
+		FormDataBodyPart filePart = form.getField("file");
+
+		ContentDisposition headerOfFilePart =  filePart.getContentDisposition();
+
+		InputStream fileInputStream = filePart.getValueAs(InputStream.class);
+
+		String nameFile = headerOfFilePart.getFileName().substring(headerOfFilePart.getFileName().lastIndexOf("."));
+		String filePath = "images/" + nameFile;
+
+		// save the file to the server
+		saveFile(fileInputStream, filePath);
+
+		String output = "File saved to server location : " + filePath;
+//		CDAO.getEntityManager().refresh(account);
+		//noinspection deprecation
+//		account.setProfilPicture(URLEncoder.encode(nameFile));
+//		CDAO.getAccountDAO().update(account);
+		return javax.ws.rs.core.Response.created(URI.create("")).build();
+	}
+
+	private void saveFile(InputStream uploadedInputStream,
+						  String serverLocation) {
+
+		try {
+			OutputStream outpuStream;
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			outpuStream = new FileOutputStream(new File(serverLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				outpuStream.write(bytes, 0, read);
+			}
+			outpuStream.flush();
+			outpuStream.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
 }
