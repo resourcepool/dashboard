f440c0535aaece5a624892b81d955542d4c2ef96
Hugo Bernardi <hugo@Air-de-Hugo.ebi.excilys.com>
2016-06-06 11:48:58 +0200

    [#1] Basic configuration for SpringBoot

---
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/SpringConfiguration.java
@@ -0,0 +1,14 @@
+package com.excilys.shooflers.dashboard.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfiguration.class, args);
+    }
+}
