4dd574937af5e44deed9f0e22c828c7dd5c52b33
VIEGAS Mickael <viegas.mickael@gmail.com>
2016-06-07 16:20:46 +0200

    [#9] Add Swagger for REST Api Docs
    Update of Bundle and Media Controllers

--- dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/rest/MediaController.java
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/rest/MediaController.java
@@ -1,7 +1,16 @@
 package com.excilys.shooflers.dashboard.server.rest;
 
+import com.excilys.shooflers.dashboard.server.dao.MediaDao;
+import com.excilys.shooflers.dashboard.server.dto.MediaMetadataDto;
+import com.excilys.shooflers.dashboard.server.dto.mapper.MediaDtoMapperImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author Loïc Ortola on 07/06/2016.
@@ -10,4 +19,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/media")
 public class MediaController {
 
+	@Autowired
+	private MediaDao mediaDao;
+
+	@Autowired
+	private MediaDtoMapperImpl mediaDtoMapper;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<MediaMetadataDto> getAll() {
+		return mediaDao.getAll().stream().map(mediaDtoMapper::toDto).collect(Collectors.toList());
+	}
+
+	@RequestMapping(value = "{uuid}", method = RequestMethod.GET)
+	public MediaMetadataDto get(@RequestParam("uuid") String uuid) {
+		return mediaDtoMapper.toDto(mediaDao.get(uuid));
+	}
 }
