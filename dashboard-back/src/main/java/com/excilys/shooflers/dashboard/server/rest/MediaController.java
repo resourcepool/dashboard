2f29c9fbe003f79dd5cd2909d5a4e077aad57b7e
Loïc Ortola <contact@loicortola.com>
2016-06-08 23:40:51 +0200

    [fix] Fixed Revision implementation. Checkstyle

--- dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/rest/MediaController.java
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/rest/MediaController.java
@@ -12,7 +12,11 @@ import com.excilys.shooflers.dashboard.server.security.annotation.RequireValidAp
 import com.excilys.shooflers.dashboard.server.security.annotation.RequireValidUser;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
