69017f5d3ac00db1a0a8e05fedf71e06b03ece14
VIEGAS Mickael <viegas.mickael@gmail.com>
2016-06-09 10:39:13 +0200

    Add intermediate interfaces for Mappers

---
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/dto/mapper/BundleDtoMapper.java
@@ -0,0 +1,10 @@
+package com.excilys.shooflers.dashboard.server.dto.mapper;
+
+import com.excilys.shooflers.dashboard.server.dto.BundleMetadataDto;
+import com.excilys.shooflers.dashboard.server.model.metadata.BundleMetadata;
+
+/**
+ * Created by Mickael on 09/06/2016.
+ */
+public interface BundleDtoMapper extends MapperDto<BundleMetadata, BundleMetadataDto> {
+}
