1466a35f48bb9f9c16af8049212279d915302c05
Hugo Bernardi <bernardihugo13@gmail.com>
2016-06-09 21:39:33 +0200

    Fixed mime type syntax error, change url of media file

--- dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/security/interceptor/CorsInterceptor.java
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/security/interceptor/CorsInterceptor.java
@@ -27,7 +27,7 @@ public class CorsInterceptor implements HandlerInterceptor {
         // Add CORS Headers to every response
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Api-Key");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Api-Key, x-api-key");
         response.setHeader("Access-Control-Expose-Headers", "Authorization");
         response.setHeader("Access-Control-Allow-Origin", "*");
 
