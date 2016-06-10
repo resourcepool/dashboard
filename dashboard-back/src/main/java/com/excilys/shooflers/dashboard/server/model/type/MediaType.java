6ad8cffffef5d65088a9488233cdbb113e73a397
bernardi hugo <bernardihugo13@gmail.com>
2016-06-10 16:50:16 +0200

    Refactoring enum MediaType

--- dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/model/type/MediaType.java
+++ dashboard-back/src/main/java/com/excilys/shooflers/dashboard/server/model/type/MediaType.java
@@ -4,27 +4,41 @@ package com.excilys.shooflers.dashboard.server.model.type;
  * Type of a media.
  */
 public enum MediaType {
-    IMAGE_PNG("image/png"),
-    IMAGE_JPG("image/jpeg"),
-    IMAGE_GIF("image/gif"),
-    VIDEO_MPEG("video/mpeg"),
-    VIDEO_MP4("video/mp4"),
-    VIDEO_AVI("video/x-msvideo"),
-    VIDEO_FLV("video/x-flv"),
-    PDF("application/pdf"),
-    POWERPOINT("application/vnd.ms-powerpoint"),
-    WEB_SITE("application/web-site"),
-    WEB_VIDEO("application/web-video"),
-    NONE("none");
+    IMAGE_PNG("image/png", ".png", "image"),
+    IMAGE_JPG("image/jpeg", ".jpg", "image"),
+    IMAGE_GIF("image/gif", ".gif", "image"),
+    VIDEO_MPEG("video/mpeg", ".mpg", "video"),
+    VIDEO_MP4("video/mp4", ".mp4", "video"),
+    VIDEO_AVI("video/x-msvideo", ".avi", "video"),
+    VIDEO_FLV("video/x-flv", ".flv", "video"),
+    PDF("application/pdf", ".pdf", "document"),
+    POWERPOINT("application/vnd.ms-powerpoint", ".pptx", "document"),
+    WEB_SITE("application/web-site", null, "web"),
+    WEB_VIDEO("application/web-video", null, "web"),
+    NONE("none", null, "none");
 
     private String mimeType;
 
-    MediaType(String mimeType) {
+    private String extension;
+
+    private String globalType;
+
+    MediaType(String mimeType, String extension, String globalType) {
         this.mimeType = mimeType;
+        this.extension = extension;
+        this.globalType = globalType;
     }
 
     public String getMimeType() {
         return mimeType;
+    }
+
+    public String getExtension() {
+        return extension;
+    }
+
+    public String getGlobalType() {
+        return globalType;
     }
 
     /**
@@ -33,49 +47,17 @@ public enum MediaType {
      * @return MediaType
      */
     public static MediaType getMediaType(String mimeType) {
-        MediaType mediaType;
+        MediaType mediaType = NONE;
         if (mimeType == null) {
-            return NONE;
+            return mediaType;
         }
 
-        switch (mimeType) {
-            case "image/png":
-                mediaType = IMAGE_PNG;
+        for (MediaType m : MediaType.values()) {
+            if (mimeType.equals(m.getMimeType())) {
+                mediaType = m;
                 break;
-            case "image/jpeg":
-                mediaType = IMAGE_JPG;
-                break;
-            case "image/gif":
-                mediaType = IMAGE_GIF;
-                break;
-            case "video/mpeg":
-                mediaType = VIDEO_MPEG;
-                break;
-            case "video/mp4":
-                mediaType = VIDEO_MP4;
-                break;
-            case "video/avi":
-                mediaType = VIDEO_AVI;
-                break;
-            case "video/flv":
-                mediaType = VIDEO_FLV;
-                break;
-            case "application/pdf":
-                mediaType = PDF;
-                break;
-            case  "application/vnd.ms-powerpoint":
-                mediaType = POWERPOINT;
-                break;
-            case "application/web-site":
-                mediaType = WEB_SITE;
-                break;
-            case "application/web-video":
-                mediaType = WEB_VIDEO;
-                break;
-            default:
-                mediaType = NONE;
+            }
         }
-
         return mediaType;
     }
 
@@ -85,39 +67,17 @@ public enum MediaType {
      * @return Extension
      */
     public static String getExtension(String mimeType) {
-        String extension;
-        switch (mimeType) {
-            case "image/png":
-                extension = ".png";
-                break;
-            case "image/jpeg":
-                extension = ".jpg";
-                break;
-            case "image/gif":
-                extension = ".gif";
-                break;
-            case "video/mpeg":
-                extension = ".mpg";
-                break;
-            case "video/mp4":
-                extension = ".mp4";
-                break;
-            case "video/avi":
-                extension = ".avi";
-                break;
-            case "video/flv":
-                extension = ".flv";
-                break;
-            case "application/pdf":
-                extension = ".pdf";
-                break;
-            case  "application/vnd.ms-powerpoint":
-                extension = ".pptx";
-                break;
-            default:
-                extension = "";
+        String extension = "";
+        if (mimeType == null) {
+            return extension;
         }
 
+        for (MediaType m : MediaType.values()) {
+            if (mimeType.equals(m.getMimeType())) {
+                extension = m.getExtension();
+                break;
+            }
+        }
         return extension;
     }
 }
