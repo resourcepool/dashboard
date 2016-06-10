d37adf6bea1e7c4358d2a5b6953af8f15ae5a3a8
Tommy Buonomo <tommy.buonomo@gmail.com>
2016-06-10 17:57:03 +0200

    [#44] Implement SplashScreenFragment

--- dashboard-tv/app/src/main/java/com/excilys/shoofleurs/dashboard/ui/activities/DashboardActivity.java
+++ dashboard-tv/app/src/main/java/com/excilys/shoofleurs/dashboard/ui/activities/DashboardActivity.java
@@ -1,10 +1,7 @@
 package com.excilys.shoofleurs.dashboard.ui.activities;
 
-import android.animation.AnimatorSet;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
+import android.support.v4.app.FragmentManager;
 
 import com.excilys.shoofleurs.dashboard.R;
 import com.excilys.shoofleurs.dashboard.model.entities.Media;
@@ -13,18 +10,19 @@ import com.excilys.shoofleurs.dashboard.ui.adapters.MediaPagerAdapter;
 import com.excilys.shoofleurs.dashboard.ui.displayables.AbstractDisplayable;
 import com.excilys.shoofleurs.dashboard.ui.displayables.Displayable;
 import com.excilys.shoofleurs.dashboard.ui.displayables.DisplayableFactory;
-import com.excilys.shoofleurs.dashboard.ui.factories.AnimatorFactory;
+import com.excilys.shoofleurs.dashboard.ui.fragments.NewsFragment;
+import com.excilys.shoofleurs.dashboard.ui.fragments.SplashScreenFragment;
 import com.excilys.shoofleurs.dashboard.ui.presenters.DashboardPresenter;
 import com.excilys.shoofleurs.dashboard.ui.utils.AndroidUtils;
 import com.excilys.shoofleurs.dashboard.ui.utils.ViewPagerCustomDuration;
 import com.excilys.shoofleurs.dashboard.ui.utils.ZoomOutPageTransformer;
 import com.excilys.shoofleurs.dashboard.ui.views.DashboardView;
+import com.excilys.shoofleurs.dashboard.ui.views.SplashScreenView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
-import butterknife.BindViews;
 import butterknife.ButterKnife;
 
 /**
@@ -32,30 +30,21 @@ import butterknife.ButterKnife;
  * It asks the server for bundles updates via the BundleService and
  * displays them.
  */
-public class DashboardActivity extends FragmentActivity implements DashboardView {
+public class DashboardActivity extends FragmentActivity implements DashboardView,
+        SplashScreenFragment.OnSplashScreenFragmentInteraction {
     private static final String TAG = "DashboardActivity";
-    private AnimatorSet[] mProgressAnimators = new AnimatorSet[4];
-    /**
-     * The waiting view points
-     */
-    @BindViews({R.id.progress_view_top_left_point,
-            R.id.progress_view_top_right_point,
-            R.id.progress_view_bottom_right_point,
-            R.id.progress_view_bottom_left_point})
-    View[] mProgressPointViews;
-
-    @BindView(R.id.background_layout)
-    RelativeLayout mBackgroundLayout;
-
-    @BindView(R.id.debug_message)
-    TextView mDebugTextView;
 
     @BindView(R.id.view_pager)
     ViewPagerCustomDuration mViewPager;
 
     private MediaPagerAdapter mAdapter;
-
     private DashboardPresenter mDashboardPresenter;
+    private SplashScreenView mSplashScreenView;
+
+    private FragmentManager mFragmentManager;
+
+    private SplashScreenFragment mSplashScreenFragment;
+    private NewsFragment mNewsFragment;
 
     @Override
     protected void onCreate(android.os.Bundle savedInstanceState) {
@@ -67,6 +56,7 @@ public class DashboardActivity extends FragmentActivity implements DashboardView
         mDashboardPresenter.attachView(this);
 
         setUpViewPager();
+        setUpFragments();
     }
 
     private void setUpViewPager() {
@@ -75,6 +65,15 @@ public class DashboardActivity extends FragmentActivity implements DashboardView
         mViewPager.addOnPageChangeListener(mAdapter);
         mViewPager.setAdapter(mAdapter);
         mViewPager.setPageTransformer(true, new ZoomOutPageTransformer());
+    }
+
+    private void setUpFragments() {
+        mFragmentManager = getSupportFragmentManager();
+        mSplashScreenFragment = (SplashScreenFragment) mFragmentManager.findFragmentById(R.id.splash_fragment);
+        mNewsFragment = (NewsFragment) mFragmentManager.findFragmentById(R.id.news_fragment);
+
+        mFragmentManager.beginTransaction().hide(mNewsFragment).commit();
+
     }
 
     @Override
@@ -107,36 +106,6 @@ public class DashboardActivity extends FragmentActivity implements DashboardView
         }
     }
 
-    /**
-     * Start the waiting animation
-     */
-    @Override
-    public void showWaitingAnimation(boolean show) {
-        if (show) {
-            RelativeLayout progressViewLayout = (RelativeLayout) findViewById(R.id.progress_view_layout);
-            mProgressAnimators[0] = AnimatorFactory.createSquarePointAnimatorSet(mProgressPointViews[0], progressViewLayout.getLayoutParams().width, 300, 0.07f, AnimatorFactory.Position.TOP_LEFT);
-            mProgressAnimators[1] = AnimatorFactory.createSquarePointAnimatorSet(mProgressPointViews[3], progressViewLayout.getLayoutParams().width, 300, 0.07f, AnimatorFactory.Position.BOTTOM_LEFT);
-            mProgressAnimators[2] = AnimatorFactory.createSquarePointAnimatorSet(mProgressPointViews[2], progressViewLayout.getLayoutParams().width, 300, 0.07f, AnimatorFactory.Position.BOTTOM_RIGHT);
-            mProgressAnimators[3] = AnimatorFactory.createSquarePointAnimatorSet(mProgressPointViews[1], progressViewLayout.getLayoutParams().width, 300, 0.07f, AnimatorFactory.Position.TOP_RIGHT);
-
-            AndroidUtils.setVisibility(View.VISIBLE, mProgressPointViews);
-            AndroidUtils.startAnimators(mProgressAnimators);
-        } else {
-            AndroidUtils.cancelAnimators(mProgressAnimators[0]);
-            AndroidUtils.setVisibility(View.GONE, mProgressPointViews);
-        }
-    }
-
-    @Override
-    public void showBackground(boolean show) {
-        mBackgroundLayout.setVisibility(show ? View.VISIBLE : View.INVISIBLE);
-    }
-
-    @Override
-    public void displayDebugMessage(int messageId) {
-        mDebugTextView.setText(getResources().getString(messageId));
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
@@ -147,5 +116,9 @@ public class DashboardActivity extends FragmentActivity implements DashboardView
     protected void onResume() {
         super.onResume();
         mDashboardPresenter.onResume();
+    }
+
+    @Override
+    public void onSplashScreenFinish() {
     }
 }
